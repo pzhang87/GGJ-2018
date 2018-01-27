a6a22ef5aa92b3fae77da71293871568c559a73d
Audrey Gu <audreymgu@gmail.com>
2018-01-27 14:00:18 -0500

    Added basic player movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,24 +7,45 @@ public class PlayerController : MonoBehaviour {
 	public float walkSpeed = 7f;
 	public float jumpSpeed = 9f;
 
+	Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-
+		rb = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
 	void Update () {
+		MoveHandler();
+	}
+
+	void MoveHandler() {
+		rb.velocity = new Vector3(0, rb.velocity.y, 0);
+
+		float distance = walkSpeed * Time.deltaTime;
+
 		// Input on x ("Horizontal")
-    float hAxis = Input.GetAxis("Horizontal");
+		float hAxis = Input.GetAxis("Horizontal");
 
-    print("Horizontal axis");
-    print(hAxis);
+		print("Horizontal axis");
+		print(hAxis);
 
-    // Input on z ("Vertical")
-    float vAxis = Input.GetAxis("Vertical");
+		// Input on z ("Vertical")
+		float vAxis = Input.GetAxis("Vertical");
 
-    print("Vertical axis");
-    print(vAxis);
+		print("Vertical axis");
+		print(vAxis);
 
+		// Movement vector
+		Vector3 movement = new Vector3(hAxis * distance, 0f, vAxis * distance);
+
+		// Current position
+		Vector3 currPosition = transform.position;
+
+		// Updated position
+		Vector3 newPosition = currPosition + movement;
+
+		// Apply to rigid body
+		rb.MovePosition(newPosition);
 	}
 }
