9356ddc5f2975c0bee8a09ab5da9e2bc57da8dee
Audrey Gu <audreymgu@gmail.com>
2018-01-27 13:40:15 -0500

    Added protoyping assets.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float walkSpeed = 7f;
+	public float jumpSpeed = 9f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Input on x ("Horizontal")
+    float hAxis = Input.GetAxis("Horizontal");
+
+    print("Horizontal axis");
+    print(hAxis);
+
+    // Input on z ("Vertical")
+    float vAxis = Input.GetAxis("Vertical");
+
+    print("Vertical axis");
+    print(vAxis);
+
+	}
+}
