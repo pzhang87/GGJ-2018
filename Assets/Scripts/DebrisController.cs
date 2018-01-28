d10619642e4a5f983dab0b360730fdd35d589c96
Peter Zhang <pzhang87@gmail.com>
2018-01-28 13:45:23 -0500

    add debris to map and work on collision/explosion code

---
+++ Assets/Scripts/DebrisController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DebrisController : NetworkBehaviour {
+
+	Collider collider;
+
+	public float rotationSpeed = 100f;
+
+	public float radius = 5.0F;
+	public float power = 10.0F;
+
+	void Start(){
+		collider = GetComponent<Collider> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	  //distance (in angles) to rotate on each frame. distance = speed * time
+	  float angle = rotationSpeed * Time.deltaTime;
+
+	  //rotate on Y
+	  transform.Rotate(Vector3.up * angle, Space.World);
+	}
+
+	void onCollisionEnter(Collision collision){
+		Vector3 explosionPos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+		foreach (Collider hit in colliders) {
+			Rigidbody rb = hit.GetComponent<Rigidbody> ();
+
+			if (rb != null)
+				rb.AddExplosionForce (power, explosionPos, radius, 3.0F);
+		}
+
+		Destroy (this);
+	}
+}
