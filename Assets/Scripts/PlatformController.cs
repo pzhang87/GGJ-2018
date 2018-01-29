ace292042efbef3b1bdaa6278adb743b1dc52514
Peter Zhang <pzhang87@gmail.com>
2018-01-29 12:12:36 -0500

    networked platforms

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlatformController : MonoBehaviour {
+public class PlatformController : NetworkBehaviour {
 
 	public Transform platform;
 	public Transform startTransform;
